541b7756e3a57476aa861bf80136cb1c385126d4
Recal <iamrecal10@gmail.com>
2026-08-22 00:04:08 +0700

    fix: fixing error in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { AuthStrategy } from './guard/auth.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '8h' }, // Token valid selama 8 jam
+      }),
+    }),
+  ],
+  providers: [AuthService, AuthStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
