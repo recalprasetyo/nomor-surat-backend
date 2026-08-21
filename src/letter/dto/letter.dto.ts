00d550cc343464ce09aa24357ba9952970a862ba
Recal <iamrecal10@gmail.com>
2026-08-22 00:20:35 +0700

    feat: add dto letter

---
+++ src/letter/dto/letter.dto.ts
@@ -0,0 +1,26 @@
+import { IsString, IsNotEmpty, IsIn } from 'class-validator';
+
+export class CreateLetterDto {
+  // type
+  @IsString({ message: 'Letter Type must be a string' })
+  @IsNotEmpty({ message: 'Letter Type is required' })
+  @IsIn(['Fasilitatif', 'Substantif'], {
+    message: 'Type must be either Fasilitatif or Substantif',
+  })
+  type!: string;
+
+  // code clasification
+  @IsString()
+  @IsNotEmpty({ message: 'Code Classification is required' })
+  code!: string;
+
+  // regarding
+  @IsString({ message: 'Regarding must be a string' })
+  @IsNotEmpty({ message: 'Regarding is required' })
+  regarding!: string;
+
+  // note
+  @IsString({ message: 'Note must be a string' })
+  @IsNotEmpty({ message: 'Note is required' })
+  note!: string;
+}
