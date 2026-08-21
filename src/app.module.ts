b7978f3b3a2d4f046cab43d6140cd5a7b47b5dca
Recal <iamrecal10@gmail.com>
2026-08-22 00:19:46 +0700

    feat: add letter controller, module, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { LetterModule } from './letter/letter.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    LetterModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
